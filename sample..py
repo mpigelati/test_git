9b9a17c65b8bddc18ba3bd15e19645dd760d9f57
mpigelati <saimohansai@gmail.com>
2017-08-21 13:19:16 +0530

    testing

--- sample..py
+++ sample..py
@@ -1,42 +1,27 @@
-
-import json
 import subprocess
-
+import json
 print("hellow world")
-"""
 gitlog= "git log > git_log.txt"
 subprocess.call(gitlog,shell=True)
-
-'''with open('js_data.json','r') as js:
-    print(js)
-
-
-def Fun_json_print():
-    with open("data.json","r") as pd:
-        data = json.load(pd)
-        print(data)
-    #for line in data:
-    #  print(line["commit"])
 
 def Fun_json(my_list):
     print("mylist",my_list)
     global count
     #print("my_list_len",len(my_list))
+    my_json = {}
     my_json = []
 
     my_json.append(
           {
-                'commit':my_list[0].split(":")[0],
-                'Auther':my_list[1],
-                'Date' :my_list[2],
-                'Text':my_list[3].lstrip(" ")
+               'Commit':my_list[0].split(" ")[1].rstrip("\n"),
+               'Auther':my_list[1].split(":")[1].rstrip("\n"),
+               'Date': my_list[2].split(":")[1].rstrip("\n"),
+               'Dev_Comment': my_list[3].lstrip(" ")
             }
         )
-    with open("data.json", 'a+')as fd:
+    with open("data.json", 'W')as fd:
         #print(fd)
-        json.dump(data,fd)'''
-
-
+        json.dump(data,fd,indent=4)
 
 with open('git_log.txt','r') as fd:
     print(fd)
@@ -50,8 +35,6 @@ for i, line in enumerate(data):
         for j in range(i,i+5):
             if(data[j] != "\n"):
                 t_list.append(data[j])
-        #Fun_json(t_list)
+        Fun_json(t_list)
         #print(t_list)
         t_list.clear()
-
-#Fun_json_print()"""
